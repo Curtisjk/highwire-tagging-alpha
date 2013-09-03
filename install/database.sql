373528e5e92df0b9edad676e4f0abfd59d679707
Curtis Kennington <curtis@curtiskennington.co.uk>
2013-09-03 13:02:18 +0100

    Regenerate SQL - tidy up config file

--- install/database.sql
+++ install/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 03, 2013 at 11:50 AM
+-- Generation Time: Sep 03, 2013 at 02:01 PM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.10
 
@@ -25,8 +25,6 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `comment`
 --
--- Creation: Sep 03, 2013 at 09:43 AM
---
 
 CREATE TABLE `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -41,9 +39,6 @@ CREATE TABLE `comment` (
 
 --
 -- Table structure for table `email`
---
--- Creation: Sep 03, 2013 at 09:43 AM
--- Last update: Sep 03, 2013 at 09:43 AM
 --
 
 CREATE TABLE `email` (
@@ -61,8 +56,6 @@ CREATE TABLE `email` (
 --
 -- Table structure for table `session`
 --
--- Creation: Sep 03, 2013 at 09:43 AM
---
 
 CREATE TABLE `session` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -71,14 +64,12 @@ CREATE TABLE `session` (
   `expertise` int(11) NOT NULL,
   `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=353 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=359 ;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
---
--- Creation: Sep 03, 2013 at 09:43 AM
 --
 
 CREATE TABLE `user` (
@@ -86,25 +77,7 @@ CREATE TABLE `user` (
   `group` int(11) DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `videos`
---
--- Creation: Sep 03, 2013 at 09:43 AM
--- Last update: Sep 03, 2013 at 09:43 AM
---
-
-CREATE TABLE `videos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `video_id` varchar(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `desc` varchar(1000) NOT NULL,
-  `length` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- public_html/config/config.php
+++ public_html/config/config.php
@@ -2,14 +2,22 @@
 	//MySQL Database Settings
 	define('DB_HOST', 'localhost');
 	define('DB_PORT', 3306);
-	define('DB_USER', 'root');
-	define('DB_PASS', 'root');
-	define('DB_NAME', 'tagging_alpha');
-	define('SITE_HOME', '/dev/taggingalpha/public_html/');
 
+	//The MySQL Username
+	define('DB_USER', '');
+
+	//The MySQL Password
+	define('DB_PASS', '');
+
+	//The MySQL Database Name
+	define('DB_NAME', '');
+
+	//The SITE_HOME setting refers to where your install is located on your site
+	//eg - http://example.com/tagging/tool/ would be '/tagging/tool/'
+	define('SITE_HOME', '/');
+
+	//The AES key is used to encrypt stored emails
 	define('AES', '5Aqeyu');
-
-	//app configuration settings
 
 	//cookies
 	define('COOKIE_LENGTH', time() + (3600 * 24)); //length, in seconds (time() + 3600 = 1 hour)
